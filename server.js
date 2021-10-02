7297af7a611c922b3a1ed7c46b3457c71e038e19
Gunjan Raj Tiwari <im.gunjan1@gmail.com>
2021-10-02 19:31:34 +0545

    sql injection setup

--- server.js
+++ server.js
@@ -1,9 +1,56 @@
 const express = require("express");
 
 const app = express();
+app.use(express.urlencoded({ extended: false }));
 
+// Database connection
+const { Client } = require("pg");
+
+const db = new Client({
+	user: "postgres",
+	password: "gunjan",
+	host: "localhost",
+	port: 5432,
+	database: "postgres",
+});
+
+db.connect();
+
+//Transfer coin
+async function transferCoin(from, to, amount) {
+	try {
+		await db.query(begin);
+		const text = "update profile set coin=coin+$1 where id = '$2'";
+	} catch (e) {
+		await db.query("rollback");
+		console.log(e);
+	}
+}
+
+// Routes
 app.get("/", (req, res) => {
 	res.send("Success");
 });
+
+app.get("/:id", async (req, res) => {
+	const id = req.params.id;
+	try {
+		// Wrong way
+		const data = await db.query(
+			`select name, email, coin from profile where email = '${id}'`
+		);
+
+		// Right Way
+		// const data = await db.query(
+		// 	`select name, email, coin from profile where email = $1`,
+		// 	[id]
+		// );
+		res.json(data);
+	} catch (e) {
+		res.json({ error: e });
+	}
+});
+
+app.post("/login");
 
 app.listen(8000);
