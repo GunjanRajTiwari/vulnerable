1de57cf876f64a16e3003e173877a6206f44b457
Gunjan Raj Tiwari <im.gunjan1@gmail.com>
2021-11-11 18:41:54 +0545

    csrf attack added

--- server.js
+++ server.js
@@ -1,8 +1,29 @@
 const express = require("express");
+const session = require("express-session");
 
 const app = express();
-app.use(express.urlencoded({ extended: false }));
+const sessions = {};
+
+// Middlewares
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static("views"));
+app.use(
+	session({
+		secret: "Session Secret",
+		resave: false,
+		saveUninitialized: false,
+	})
+);
+
+// Custom middleware
+function authenticate(req, res, next) {
+	if (sessions[req.session.id] != undefined) {
+		console.log("You are logged in!");
+		return next();
+	}
+	console.log("Please login");
+	return res.redirect("/login");
+}
 
 // Database connection
 const { Client } = require("pg");
@@ -21,7 +42,7 @@ db.connect();
 async function transferCoin(from, to, amount) {
 	try {
 		await db.query("begin");
-		const text = "update profile set coin=coin+$1 where id = '$2'";
+		const text = "update profile set coin=coin+$1 where id = $2";
 		await db.query(text, [amount, to]);
 		await db.query(text, [-amount, from]);
 		await db.query("commit");
@@ -32,27 +53,12 @@ async function transferCoin(from, to, amount) {
 }
 
 // Routes
-app.get("/", (req, res) => {
+app.get("/login", (req, res) => {
 	res.sendFile(__dirname + "/views/login.html");
 });
 
-app.get("/:id", async (req, res) => {
-	const id = req.params.id;
-	try {
-		// Wrong way
-		const data = await db.query(
-			`select id, name, email, coin from profile where id = ${id}`
-		);
-
-		// Right Way
-		// const data = await db.query(
-		// 	`select id, name, email, coin from profile where id = $1`,
-		// 	[id]
-		// );
-		res.json(data.rows);
-	} catch (e) {
-		res.json({ error: e });
-	}
+app.get("/", authenticate, (req, res) => {
+	res.sendFile(__dirname + "/views/home.html");
 });
 
 app.post("/login", async (req, res) => {
@@ -66,14 +72,41 @@ app.post("/login", async (req, res) => {
 	}
 
 	try {
-		const text = "select password from profile where email = $1";
+		const text = "select id, password from profile where email = $1";
 		const response = await db.query(text, [email]);
 		const actualPassword = response.rows[0].password;
 		if (password == actualPassword) {
-			return res.json({ authenticated: true });
+			req.session.authUser = response.rows[0].id;
+			sessions[req.session.id] = req.session;
+			console.log(sessions);
+			return res.redirect("/");
 		}
 		console.log(actualPassword);
 		res.json({ error: "Invalid Credentials" });
+	} catch (e) {
+		res.json({ error: e });
+	}
+});
+
+app.post("/sendCoins", authenticate, async (req, res) => {
+	await transferCoin(req.session.authUser, req.body.to, req.body.amount);
+	res.send();
+});
+
+app.get("/:id", async (req, res) => {
+	const id = req.params.id;
+	try {
+		// Wrong way
+		const data = await db.query(
+			`select id, name, email, password, coin from profile where id = ${id}`
+		);
+
+		// Right Way
+		// const data = await db.query(
+		// 	`select id, name, email, coin from profile where id = $1`,
+		// 	[id]
+		// );
+		res.json(data.rows);
 	} catch (e) {
 		res.json({ error: e });
 	}
